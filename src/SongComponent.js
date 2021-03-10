36658f96388ad346389fc45cf56ae2ee39721ca6
meru20 <merhawitgubsa@gmail.com>
2021-03-09 22:43:15 -0800

    (feat): added link for the youtubelink

--- src/SongComponent.js
+++ src/SongComponent.js
@@ -7,7 +7,7 @@ const SongComponent = (props) => {
             <h2><a href={props.song.youtube}>{props.song.title}</a></h2>
             <h3>{props.song.genre}</h3>
             <h3>{props.song.year}</h3>
-            <h3>{props.song.youtube}</h3>
+            <h3><a href={props.song.youtube}>{props.song.youtube}</a></h3>
         </div>
     )
 
