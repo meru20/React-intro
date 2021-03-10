b1967d320bdae237d276f75509166a5a0da0ad98
meru20 <merhawitgubsa@gmail.com>
2021-03-10 10:30:41 -0800

    (fix):ability to open the tab on another window

--- src/SongComponent.js
+++ src/SongComponent.js
@@ -4,7 +4,7 @@ const SongComponent = (props) => {
         <div className="text-center">
             <h1> My Favorit song</h1>
             <h2>{props.song.artist}</h2>
-            <h2><a href={props.song.youtube}>{props.song.title}</a></h2>
+            <h2><a href={props.song.youtube} target='_blank' rel='noreferrer'>{props.song.title}</a></h2>
             <h3>{props.song.genre}</h3>
             <h3>{props.song.year}</h3>
             <h3><a href={props.song.youtube}>{props.song.youtube}</a></h3>
